56e4c210b648851702d64e852e4ee15d95a168f4
Yuxin Wang <Wangww202306@163.com>
2023-06-13 12:24:29 +0200

    complete feature development

--- src/main/java/com/example/softwarelifecyclemanagement/controller/CalculateController.java
+++ src/main/java/com/example/softwarelifecyclemanagement/controller/CalculateController.java
@@ -3,7 +3,9 @@ package com.example.softwarelifecyclemanagement.controller;
 import com.example.softwarelifecyclemanagement.service.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +23,14 @@ public class CalculateController {
     @ResponseBody
     public String getPrevious() {
         return calculateService.getPrevious();
+    }
+
+    @RequestMapping("/")
+    public String index(Model model, @RequestParam(value = "string", required = false) String string) {
+        model.addAttribute("previous", calculateService.getPrevious());
+        if (string != null) {
+            model.addAttribute("string", calculateService.calculate(string));
+        }
+        return "index";
     }
 }
