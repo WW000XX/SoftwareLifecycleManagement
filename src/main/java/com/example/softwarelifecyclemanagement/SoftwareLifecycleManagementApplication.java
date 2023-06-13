5f343c52b0655f90e44a0d17870004305d184069
Yuxin Wang <Wangww202306@163.com>
2023-06-13 11:29:23 +0200

    Initial commit

---
+++ src/main/java/com/example/softwarelifecyclemanagement/SoftwareLifecycleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwarelifecyclemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareLifecycleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareLifecycleManagementApplication.class, args);
+    }
+
+}
